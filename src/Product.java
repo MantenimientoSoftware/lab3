f3b8f7a5995350320c7c459f4f7333d0a1c4d137
ivancastel <ivancastel94@gmail.com>
2015-06-19 08:00:45 -0500

    Corrections on Product and Changes in User

--- src/Product.java
+++ src/Product.java
@@ -2,65 +2,54 @@
  * Created by usuario on 19/06/2015.
  */
 public class Product {
-    String name;
-    String email;
-    String username;
-    String password;
-    String loginstatus;
+    int productid;
+    String category;
+    String description;
+    String status;
 
     public Product() {
-        this.name = "";
-        this.email = "";
-        this.username = "";
-        this.password = "";
-        this.loginstatus = "";
+        this.productid = 0;
+        this.category = "";
+        this.description = "";
+        this.status = "";
     }
 
-    public Product(String name, String email, String username, String password, String loginstatus) {
-        this.name = name;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-        this.loginstatus = loginstatus;
+    public Product(int productid, String category, String description, String status) {
+        this.productid = productid;
+        this.category = category;
+        this.description = description;
+        this.status = status;
     }
 
-    public String getName() {
-        return name;
+    public int getProductid() {
+        return productid;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setProductid(int productid) {
+        this.productid = productid;
     }
 
-    public String getEmail() {
-        return email;
+    public String getCategory() {
+        return category;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public String getUsername() {
-        return username;
+    public String getDescription() {
+        return description;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getPassword() {
-        return password;
+    public String getStatus() {
+        return status;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getLoginstatus() {
-        return loginstatus;
-    }
-
-    public void setLoginstatus(String loginstatus) {
-        this.loginstatus = loginstatus;
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
--- src/User.java
+++ src/User.java
@@ -2,4 +2,65 @@
  * Created by usuario on 19/06/2015.
  */
 public class User {
+    String name;
+    String email;
+    String username;
+    String password;
+    String loginstatus;
+
+    public User() {
+        this.name = "";
+        this.email = "";
+        this.username = "";
+        this.password = "";
+        this.loginstatus = "";
+    }
+
+    public User(String name, String email, String username, String password, String loginstatus) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.loginstatus = loginstatus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLoginstatus() {
+        return loginstatus;
+    }
+
+    public void setLoginstatus(String loginstatus) {
+        this.loginstatus = loginstatus;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
