f79b524eff87cf64fa0e18363e4b3fa7680f67f7
ivancastel <ivancastel94@gmail.com>
2015-06-19 07:46:19 -0500

    changes in administrator and customer

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,5 +1,33 @@
 /**
  * Created by usuario on 19/06/2015.
  */
-public class Administrator {
+public class Administrator extends User {
+    String[] Permissions;
+    String[] Role;
+
+    public Administrator() {
+        this.Permissions = null;
+        this.Role = null;
+    }
+
+    public Administrator(String[] permissions, String[] role) {
+        this.Permissions = permissions;
+        this.Role = role;
+    }
+
+    public String[] getPermissions() {
+        return Permissions;
+    }
+
+    public void setPermissions(String[] permissions) {
+        Permissions = permissions;
+    }
+
+    public String[] getRole() {
+        return Role;
+    }
+
+    public void setRole(String[] role) {
+        Role = role;
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,44 @@
 /**
  * Created by usuario on 19/06/2015.
  */
-public class Customer {
+public class Customer extends User{
+    long creditcard;
+    String[] interests;
+    String postaladress;
+
+    public Customer() {
+        this.creditcard = 0;
+        this.interests = null;
+        this.postaladress = "";
+    }
+
+    public Customer(long creditcard, String[] interests, String postaladress) {
+        this.creditcard = creditcard;
+        this.interests = interests;
+        this.postaladress = postaladress;
+    }
+
+    public long getCreditcard() {
+        return creditcard;
+    }
+
+    public void setCreditcard(long creditcard) {
+        this.creditcard = creditcard;
+    }
+
+    public String[] getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String[] interests) {
+        this.interests = interests;
+    }
+
+    public String getPostaladress() {
+        return postaladress;
+    }
+
+    public void setPostaladress(String postaladress) {
+        this.postaladress = postaladress;
+    }
 }
